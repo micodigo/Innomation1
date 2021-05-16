f599fc437c78b612be365017671201056ea4c856
mayank2gupta <38257969+mayank2gupta@users.noreply.github.com>
2021-05-16 16:59:02 +0530

    header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../img/logo6.png";
+import logo from "../img/logo.png";
+import picon from "../img/phone-icon.png";
 import "./Header.scss";
-
+import { useState } from "react";
 export default function Header() {
+  const [navbar, setnavbar] = useState(false);
+  const headerHandler = () => {
+    if (window.scrollY >= 150) {
+      setnavbar(true);
+    } else {
+      setnavbar(false);
+    }
+  };
+  window.addEventListener("scroll", headerHandler);
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={logo} alt="" className="logo-img" />
+    <>
+      <div className="header-sub">
+        <div className="logo">
+          <img src={logo} alt="" className="logo-img" />
+        </div>
+        <i className="header-sub-phone">
+          <img src={picon} alt="" className="p-icon" /> +919876346722
+        </i>
+        <Link to="/ContactUs" className="header-sub-contact">
+          Contact Us
+        </Link>
       </div>
-      <nav className="navbar">
-        <ul className="navbar-links">
-          <li>
-            <Link to="/" className="link">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/AboutUs" className="link">
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link to="Services" className="link">
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link to="ContactUs" className="link">
-              Contact Us
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <hr className="header-partition" />
+      <div
+        className={navbar ? "header-main header-main-scroll" : "header-main"}
+      >
+        <nav className="navbar">
+          <ul className="navbar-links">
+            <li>
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/AboutUs" className="link">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="/Services" className="link">
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="/ContactUs" className="link">
+                Contact Us
+              </Link>
+            </li>
+            <li>
+              <Link to="/Services" className="link">
+                Services
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
