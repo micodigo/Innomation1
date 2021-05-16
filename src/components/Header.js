8b0faafb891105457cb1e12016e9814540bbb1c0
mayank2gupta <38257969+mayank2gupta@users.noreply.github.com>
2021-05-16 19:16:28 +0530

    header improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import logo from "../img/logo.png";
 import picon from "../img/phone-icon.png";
 import "./Header.scss";
 import { useState } from "react";
+import "../themify-icons.css";
+
 export default function Header() {
   const [navbar, setnavbar] = useState(false);
   const headerHandler = () => {
@@ -34,32 +36,46 @@ export default function Header() {
         <nav className="navbar">
           <ul className="navbar-links">
             <li>
-              <Link to="/" className="link">
+              <Link to="/" className="navbar-links-link">
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/AboutUs" className="link">
+              <Link to="/AboutUs" className="navbar-links-link">
                 About Us
               </Link>
             </li>
             <li>
-              <Link to="/Services" className="link">
+              <Link to="/Services" className="navbar-links-link">
                 Services
               </Link>
             </li>
             <li>
-              <Link to="/ContactUs" className="link">
+              <Link to="/ContactUs" className="navbar-links-link">
                 Contact Us
               </Link>
             </li>
             <li>
-              <Link to="/Services" className="link">
+              <Link to="/Services" className="navbar-links-link">
                 Services
               </Link>
             </li>
           </ul>
         </nav>
+        <div className="social">
+          <a href="">
+            <i className="ti-facebook social-icon"></i>
+          </a>
+          <a href="">
+            <i className="ti-twitter social-icon"></i>
+          </a>
+          <a href="">
+            <i className="ti-instagram social-icon"></i>
+          </a>
+          <a href="">
+            <i className="ti-skype social-icon"></i>
+          </a>
+        </div>
       </div>
     </>
   );
